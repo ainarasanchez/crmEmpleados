27d8c6a3a7bcc66e67a9be03fee8fbe61b2ece46
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-18 21:32:01 +0100

    Petición de Login de usuario

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IUser } from '../interfaces/iuser.interface';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  private endpoint: string = "https://crm-empleados-v2.onrender.com/api/usuarios"
+  private httpClient = inject(HttpClient);
 
-  constructor() { }
+  login(user: IUser): Promise<Response> {
+    return lastValueFrom(this.httpClient.post<Response>(`${this.endpoint}/login`, user))
+  }
+
 }
