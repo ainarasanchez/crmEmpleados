9f7a7fcd49e66742b710d17b096dc9266ecbe738
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-19 20:53:43 +0100

    Añadimos los componentes a las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { loginGuard } from './guards/login.guard';
+import { EmpleadosListComponent } from './pages/dashboard/empleados-list/empleados-list.component';
+import { EmpleadoFormComponent } from './pages/dashboard/empleado-form/empleado-form.component';
+import { EmpleadoViewComponent } from './pages/dashboard/empleado-view/empleado-view.component';
 
 export const routes: Routes = [
     { path: "", pathMatch: "full", redirectTo: "home" },
@@ -11,10 +14,10 @@ export const routes: Routes = [
     { path: "dashboard", component: DashboardComponent, canActivate: [loginGuard], children: 
         [
             { path: '', pathMatch: 'full', redirectTo: 'empleados' },
-            { path: 'empleados', component:  },
-            { path: 'empleado/new', component:  },
-            { path: 'empleado/:idEmployee', component:  },
-            { path: 'empleado/update/:idEmployee', component:  }
+            { path: 'empleados', component: EmpleadosListComponent },
+            { path: 'empleado/new', component: EmpleadoFormComponent },
+            { path: 'empleado/:idEmployee', component: EmpleadoViewComponent },
+            { path: 'empleado/update/:idEmployee', component: EmpleadoFormComponent }
           ]
     },
     { path: "**", redirectTo: 'home' }
