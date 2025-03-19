a29876ad110601473cbe2d1742aa9afacec1e3f0
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-19 21:14:26 +0100

    getAll & endPoint

--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { IEmployee } from '../interfaces/iemployee.interface';
+import { lastValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeesService {
 
-  constructor() { }
+  private endPoint: string = "https://crm-empleados-v2.onrender.com/api/empleados"
+  private httpClient = inject(HttpClient);
+
+
+  getAll(): Promise<IEmployee[]> {
+    return lastValueFrom()
+  }
+
+
 }
