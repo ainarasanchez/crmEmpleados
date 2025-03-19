1ff487c85642cd74f90b47de8951d981236ff75b
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-19 21:52:41 +0100

    Probamos a ver si nos trae los empleados

--- src/app/pages/dashboard/empleados-list/empleados-list.component.ts
+++ src/app/pages/dashboard/empleados-list/empleados-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IEmployee } from '../../../interfaces/iemployee.interface';
+import { EmployeesService } from '../../../services/employees.service';
 
 @Component({
   selector: 'app-empleados-list',
@@ -7,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './empleados-list.component.css'
 })
 export class EmpleadosListComponent {
+  arrEmployees: IEmployee[] = [];
+  employeesService = inject(EmployeesService);
+
+  async ngOnInit() {
+    try {
+      this.arrEmployees = await this.employeesService.getAll();
+      console.log(this.arrEmployees);
+    } catch (error) {
+      console.error('Error fetching employees', error);
+    }
+  }
 
 }
