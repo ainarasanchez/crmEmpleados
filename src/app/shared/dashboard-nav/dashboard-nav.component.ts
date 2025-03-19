3ce655f0b2524d412f82606569b8a38b7c7ada5f
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-19 20:47:20 +0100

    RouterLink

--- src/app/shared/dashboard-nav/dashboard-nav.component.ts
+++ src/app/shared/dashboard-nav/dashboard-nav.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-nav',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './dashboard-nav.component.html',
   styleUrl: './dashboard-nav.component.css'
 })
