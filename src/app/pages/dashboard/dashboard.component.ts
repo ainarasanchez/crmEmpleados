53c347ad6094a0e5ecec4a72bf2fe768015c22b0
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-19 20:56:41 +0100

    RouterOulet

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { DashboardNavComponent } from "../../shared/dashboard-nav/dashboard-nav.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [DashboardNavComponent],
+  imports: [DashboardNavComponent, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
