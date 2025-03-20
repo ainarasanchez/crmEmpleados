9457e8ddec4e4740137af33495ac9aa327b8c76c
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-20 19:30:36 +0100

    Creamos un componente para la fila de cada empleado
    ng g c shared/rowEmployee --skip-tests

---
+++ src/app/shared/row-employee/row-employee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-row-employee',
+  imports: [],
+  templateUrl: './row-employee.component.html',
+  styleUrl: './row-employee.component.css'
+})
+export class RowEmployeeComponent {
+
+}
