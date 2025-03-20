b72d7838edef7cd66165c6cc8e756c11ee49ef66
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-20 20:22:31 +0100

    Delete employee 3.0
    cargar empleados otra vez

--- src/app/pages/dashboard/empleados-list/empleados-list.component.ts
+++ src/app/pages/dashboard/empleados-list/empleados-list.component.ts
@@ -14,12 +14,23 @@ export class EmpleadosListComponent {
   arrEmployees: IEmployee[] = [];
   employeesService = inject(EmployeesService);
 
-  async ngOnInit() {
+  ngOnInit() {
+    this.cargaEmpleados()
+  }
+
+  async cargaEmpleados() {
     try {
       this.arrEmployees = await this.employeesService.getAll();
-      // console.log(this.arrEmployees);
+      //console.log(this.arrEmployees)
     } catch (msg: any) {
-      toast.error(msg.error.error);
+      toast.error(msg.error.error)
+    }
+  }
+
+  deleteEmployee(event: Boolean) {
+    if (event) {
+      //refrescar la lista de empleados this.arrEmployees
+      this.cargaEmpleados()
     }
   }
   
