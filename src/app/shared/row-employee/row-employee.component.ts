aa53f07bfe4f88ac00f936c0fa0c9c909e9caa09
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-20 19:52:46 +0100

    Creamos y añadimos el componente de botones

--- src/app/shared/row-employee/row-employee.component.ts
+++ src/app/shared/row-employee/row-employee.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { IEmployee } from '../../interfaces/iemployee.interface';
+import { ButtonsComponent } from "../buttons/buttons.component";
 
 @Component({
   selector: '[app-row-employee]',
-  imports: [],
+  imports: [ButtonsComponent],
   templateUrl: './row-employee.component.html',
   styleUrl: './row-employee.component.css'
 })
