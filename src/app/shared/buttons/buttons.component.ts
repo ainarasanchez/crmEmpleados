ba0bce2dfadfb9f1414ae9cc92be31e0335c7ba7
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-20 19:56:47 +0100

    RouterLink

--- src/app/shared/buttons/buttons.component.ts
+++ src/app/shared/buttons/buttons.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-buttons',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './buttons.component.html',
   styleUrl: './buttons.component.css'
 })
