eefab71f01c71cd092001edbfd12a18552fdd06a
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-20 19:45:57 +0100

    Truquito
    Componente como atributo del tr

--- src/app/shared/row-employee/row-employee.component.ts
+++ src/app/shared/row-employee/row-employee.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { IEmployee } from '../../interfaces/iemployee.interface';
 
 @Component({
-  selector: 'app-row-employee',
+  selector: '[app-row-employee]',
   imports: [],
   templateUrl: './row-employee.component.html',
   styleUrl: './row-employee.component.css'
